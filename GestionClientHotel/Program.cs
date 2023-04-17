42cd3301cd80921c1c742d7e46613b3e2e75d6d6
Mohamed islem ABDOU <MohamedislemABDOU@gmail.com>
2023-04-17 00:48:04 -0400

    Ajout d'un projet vide

---
+++ GestionClientHotel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionClientHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
