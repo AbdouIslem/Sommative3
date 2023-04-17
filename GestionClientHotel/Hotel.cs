216ca420b002c6d6248df4f05ddd776199c1c0d1
Mohamed islem ABDOU <MohamedislemABDOU@gmail.com>
2023-04-17 02:07:52 -0400

    Ajout de la classe Hotel

---
+++ GestionClientHotel/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClientHotel
+{
+    internal class Hotel
+    {
+        public ArrayList  clients;
+
+        public Hotel() 
+        {
+            clients = new ArrayList();
+        }
+
+        public void AjoutClient(Client client)
+        {
+            clients.Add(client);
+        }
+    }
+}
