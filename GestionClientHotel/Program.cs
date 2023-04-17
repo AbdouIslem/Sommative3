5530bae8af742a87d7ad5328e01809e3689533e5
Mohamed islem ABDOU <MohamedislemABDOU@gmail.com>
2023-04-17 03:12:48 -0400

    version conflit

--- GestionClientHotel/Program.cs
+++ GestionClientHotel/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Conflit!");
+            Console.WriteLine("Conflit");
+            Console.WriteLine("Conflit");
         }
     }
 }
